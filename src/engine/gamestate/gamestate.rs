80fd3e914d482238ca301725146712ab6bbf70c3
SeeStarz <mfahrim7@gmail.com>
2026-03-03 00:53:24 +0700

    [REFACTOR] Refactor gamestate to live in gamestate.rs instead of mod.rs

---
+++ src/engine/gamestate/gamestate.rs
@@ -0,0 +1,39 @@
+use crate::engine::{
+    Coordinate,
+    gamestate::{board::Board, init_pieces},
+    piece::PieceExternal,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Gamestate {
+    board: Board,
+}
+
+impl Gamestate {
+    pub fn pieces(&self) -> impl Iterator<Item = PieceExternal> {
+        self.board
+            .tiles
+            .iter()
+            .enumerate()
+            .map(|(row, line)| {
+                line.iter().enumerate().filter_map(move |(col, tile)| {
+                    tile.map(|piece| {
+                        PieceExternal::from_piece(
+                            piece,
+                            Coordinate::new_unchecked(row as i32, col as i32),
+                        )
+                    })
+                })
+            })
+            .flatten()
+    }
+
+    pub fn new() -> Self {
+        let mut board = Board::new();
+        for (coordinate, piece) in init_pieces::normal() {
+            assert!(board.at(coordinate).is_none());
+            board.set_at(coordinate, Some(piece));
+        }
+        Self { board }
+    }
+}
--- src/engine/gamestate/mod.rs
+++ src/engine/gamestate/mod.rs
@@ -1,42 +1,5 @@
 mod board;
+mod gamestate;
 mod init_pieces;
 
-use crate::engine::{
-    Coordinate,
-    piece::{Piece, PieceExternal},
-};
-use board::Board;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Gamestate {
-    board: Board,
-}
-
-impl Gamestate {
-    pub fn pieces(&self) -> impl Iterator<Item = PieceExternal> {
-        self.board
-            .tiles
-            .iter()
-            .enumerate()
-            .map(|(row, line)| {
-                line.iter().enumerate().filter_map(move |(col, tile)| {
-                    tile.map(|piece| {
-                        PieceExternal::from_piece(
-                            piece,
-                            Coordinate::new_unchecked(row as i32, col as i32),
-                        )
-                    })
-                })
-            })
-            .flatten()
-    }
-
-    pub fn new() -> Self {
-        let mut board = Board::new();
-        for (coordinate, piece) in init_pieces::normal() {
-            assert!(board.at(coordinate).is_none());
-            board.set_at(coordinate, Some(piece));
-        }
-        Self { board }
-    }
-}
+pub use gamestate::*;
