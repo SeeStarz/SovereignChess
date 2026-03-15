8e38a3cbf50372197dd6987d7ee3c9fab16c1f16
SeeStarz <mfahrim7@gmail.com>
2026-03-15 14:59:36 +0700

    [FIX] Fix a bug where occupying the enemy's main color grants you ownership of said faction

--- src/engine/faction.rs
+++ src/engine/faction.rs
@@ -43,12 +43,18 @@ impl CanonicalState {
         let mut real_owners = [None; 12];
         for faction in Color::iter() {
             let mut owner = faction;
-            while let Some(next) = direct_owners[owner as usize] {
-                owner = next;
-            }
-
             if self.player_colors.iter().any(|&f| f == owner) {
                 real_owners[faction as usize] = Some(owner);
+                continue;
+            }
+
+            while let Some(next) = direct_owners[owner as usize] {
+                owner = next;
+
+                if self.player_colors.iter().any(|&f| f == owner) {
+                    real_owners[faction as usize] = Some(owner);
+                    break;
+                }
             }
         }
         real_owners
