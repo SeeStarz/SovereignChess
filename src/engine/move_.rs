71235fe02de0de512d8bd321eb4386122060af5b
SeeStarz <mfahrim7@gmail.com>
2026-03-15 15:10:43 +0700

    [REFACTOR] Move pawn direction calculation outside the move calculation to reduce complexity

--- src/engine/move_.rs
+++ src/engine/move_.rs
@@ -12,6 +12,13 @@ pub struct Move {
     pub destination: Coordinate,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct PawnMoveDirection {
+    direction: Direction,
+    double_move: bool,
+}
+type PawnAttackDirection = Direction;
+
 // BE CAREFUL ABOUT THE ORDER
 const DIRECTION_STORE: [Direction; 16] = [
     // Rook
@@ -95,8 +102,7 @@ impl Gamestate {
 
                 if let Some(victim) = self.c().board.at(destination) {
                     if self.get_allegiance(victim.faction) == Allegiance::Enemy {
-                        if (self
-                            .check_special_tile_occupibility_rules_ok(destination, piece.faction))
+                        if self.check_special_tile_occupibility_rules_ok(destination, piece.faction)
                         {
                             moves.push(Move {
                                 origin,
@@ -150,65 +156,7 @@ impl Gamestate {
         faction: faction::Color,
         origin: Coordinate,
     ) {
-        struct PawnMoveDirection {
-            direction: Direction,
-            double_move: bool,
-        }
-
-        let move_directions = {
-            let mut move_directions = Vec::new();
-            if origin.row() < 7 {
-                move_directions.push(PawnMoveDirection {
-                    direction: Direction::new(1, 0),
-                    double_move: origin.row() < 2,
-                });
-            } else if origin.row() > 8 {
-                move_directions.push(PawnMoveDirection {
-                    direction: Direction::new(-1, 0),
-                    double_move: origin.row() > 13,
-                });
-            }
-
-            if origin.col() < 7 {
-                move_directions.push(PawnMoveDirection {
-                    direction: Direction::new(0, 1),
-                    double_move: origin.col() < 2,
-                });
-            } else if origin.col() > 8 {
-                move_directions.push(PawnMoveDirection {
-                    direction: Direction::new(0, -1),
-                    double_move: origin.col() > 13,
-                });
-            }
-            move_directions
-        };
-
-        let attack_directions = {
-            let mut attack_directions = Vec::new();
-
-            if move_directions.len() == 1 {
-                let direction = move_directions[0].direction;
-                if direction.row != 0 {
-                    attack_directions.push(direction + Direction::new(0, 1));
-                    attack_directions.push(direction + Direction::new(0, -1));
-                } else {
-                    attack_directions.push(direction + Direction::new(1, 0));
-                    attack_directions.push(direction + Direction::new(-1, 0));
-                }
-            } else {
-                // A pawn is somehow in the inner 2x2 ring if false
-                assert!(move_directions.len() == 2);
-
-                let dir1 = move_directions[0].direction;
-                let dir2 = move_directions[1].direction;
-
-                attack_directions.push(dir1 + dir2);
-                attack_directions.push((-dir1) + dir2);
-                attack_directions.push(dir1 + (-dir2));
-            }
-            attack_directions
-        };
-
+        let (move_directions, attack_directions) = calculate_pawn_directions(origin);
         for direction in move_directions {
             let Some(destination) = origin.offset(direction.direction) else {
                 continue;
@@ -224,9 +172,10 @@ impl Gamestate {
                 });
             }
 
-            if direction.double_move
-                && let Some(destination) = origin.offset(direction.direction * 2)
-            {
+            if direction.double_move {
+                let Some(destination) = origin.offset(direction.direction * 2) else {
+                    continue;
+                };
                 if self.c().board.at(destination).is_some() {
                     continue;
                 }
@@ -259,6 +208,66 @@ impl Gamestate {
             }
         }
     }
+}
+
+fn calculate_pawn_directions(
+    origin: Coordinate,
+) -> (Vec<PawnMoveDirection>, Vec<PawnAttackDirection>) {
+    let move_directions = {
+        let mut move_directions = Vec::new();
+        if origin.row() < 7 {
+            move_directions.push(PawnMoveDirection {
+                direction: Direction::new(1, 0),
+                double_move: origin.row() < 2,
+            });
+        } else if origin.row() > 8 {
+            move_directions.push(PawnMoveDirection {
+                direction: Direction::new(-1, 0),
+                double_move: origin.row() > 13,
+            });
+        }
+
+        if origin.col() < 7 {
+            move_directions.push(PawnMoveDirection {
+                direction: Direction::new(0, 1),
+                double_move: origin.col() < 2,
+            });
+        } else if origin.col() > 8 {
+            move_directions.push(PawnMoveDirection {
+                direction: Direction::new(0, -1),
+                double_move: origin.col() > 13,
+            });
+        }
+        move_directions
+    };
+
+    let attack_directions = {
+        let mut attack_directions = Vec::new();
+
+        if move_directions.len() == 1 {
+            let direction = move_directions[0].direction;
+            if direction.row != 0 {
+                attack_directions.push(direction + Direction::new(0, 1));
+                attack_directions.push(direction + Direction::new(0, -1));
+            } else {
+                attack_directions.push(direction + Direction::new(1, 0));
+                attack_directions.push(direction + Direction::new(-1, 0));
+            }
+        } else {
+            // A pawn is somehow in the inner 2x2 ring if false
+            assert!(move_directions.len() == 2);
+
+            let dir1 = move_directions[0].direction;
+            let dir2 = move_directions[1].direction;
+
+            attack_directions.push(dir1 + dir2);
+            attack_directions.push((-dir1) + dir2);
+            attack_directions.push(dir1 + (-dir2));
+        }
+        attack_directions
+    };
+
+    (move_directions, attack_directions)
 }
 
 // For debug purposes, e.g. allowing a piece to move anywhere
